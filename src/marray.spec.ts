21777d10cc307405b5dcd4b19b6d1f940bb96645
Gorgi Kosev <gorgi.kosev@gmail.com>
2020-08-13 14:27:48 +0100

    add monoid test

--- src/marray.spec.ts
+++ src/marray.spec.ts
@@ -24,3 +24,57 @@ describe('array', () => {
     expect(a.reduceTo(2, Total)).toEqual(17);
   });
 });
+
+type Node = { type: 'h1' | 'h2' | 'h3' | 'p'; text: string };
+
+type ParentList = Array<Node>;
+let ParentListLength = 3;
+
+let VirtualParent = {
+  operation(a: ParentList, b: ParentList) {
+    let res = new Array(ParentListLength) as ParentList;
+    let useA = true;
+    res.fill(null);
+    for (let k = 0; k < res.length; ++k) {
+      if (b[k] != null) {
+        useA = false;
+        res[k] = b[k];
+      } else if (useA) {
+        res[k] = a[k];
+      }
+    }
+    return res;
+  },
+  getCacheValue(n: Node) {
+    let res = new Array(ParentListLength).fill(null) as ParentList;
+    if (n.type === 'p') return res;
+    let idx = Number(n.type.substr(1)) - 1;
+    res[idx] = n;
+    return res;
+  },
+  identity: [null, null, null] as ParentList,
+};
+
+describe('virtual parents', () => {
+  it('works', () => {
+    let v = MArray.from<Node>([
+      { type: 'p', text: 'p1' },
+      { type: 'h1', text: 'h1-1' },
+      { type: 'p', text: 'a' },
+      { type: 'h2', text: 'h2-1' },
+      { type: 'p', text: 'b' },
+      { type: 'h3', text: 'h3-1' },
+      { type: 'p', text: 'c' },
+      { type: 'p', text: 'd' },
+      { type: 'p', text: 'e' },
+      { type: 'h1', text: 'h1-2' },
+      { type: 'h3', text: 'h3-2' },
+      { type: 'p', text: 'f' },
+      { type: 'p', text: 'g' },
+      { type: 'p', text: 'h' },
+    ]);
+    for (let k = 7; k < 12; ++k) {
+      console.log(v.at(k), v.reduceTo(k, VirtualParent));
+    }
+  });
+});
