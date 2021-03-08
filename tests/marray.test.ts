c0ba502c47b330576e1474e3e02d166fc92f7017
M <mveljanovski@live.com>
2021-03-08 16:00:27 +0100

    Extract MAX_ARR_LENGTH const

--- tests/marray.test.ts
+++ tests/marray.test.ts
@@ -3,14 +3,16 @@ import { MArray } from '../src';
 
 describe('mArray', () => {
   it('slice should work with randomly generated splice operations', () => {
+    const ARR_MAX_LENGTH = 1000;
+
     fc.assert(
       fc.property(
-        fc.array(fc.integer(), { maxLength: 1000 }),
+        fc.array(fc.integer(), { maxLength: ARR_MAX_LENGTH }),
         fc.array(
           fc.record({
-            at: fc.integer(-2000, 2000), // So the prob a case where the index is out of bounds is between 0-50%
-            deleteCount: fc.integer(-2000, 2000), // So the prob a case where the count is out of bounds is between 0-50%
-            add: fc.array(fc.integer(), { maxLength: 1000 }),
+            at: fc.integer(-ARR_MAX_LENGTH * 2, ARR_MAX_LENGTH * 2), // So the prob a case where the index is out of bounds is between 0-50%
+            deleteCount: fc.integer(-ARR_MAX_LENGTH * 2, ARR_MAX_LENGTH * 2), // So the prob a case where the count is out of bounds is between 0-50%
+            add: fc.array(fc.integer(), { maxLength: ARR_MAX_LENGTH }),
           }),
         ),
         (data, splices) => {
